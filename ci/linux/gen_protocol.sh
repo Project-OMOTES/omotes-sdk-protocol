db7bb8ca66182eb8711a5e3ee37667464e3c44d7
Sebastiaan la Fleur <sebastiaan.lafleur@tno.nl>
2024-02-15 15:18:43 +0100

    Add Mark as author and set py.typed marker file.

--- ci/linux/gen_protocol.sh
+++ ci/linux/gen_protocol.sh
@@ -9,3 +9,4 @@ mkdir -p python/src/omotes_sdk_protocol/
 protoc -I src/ --python_out python/src/omotes_sdk_protocol/ ./src/job.proto
 protoc -I src/ --mypy_out python/src/omotes_sdk_protocol/ ./src/job.proto
 touch python/src/omotes_sdk_protocol/__init__.py
+touch python/src/omotes_sdk_protocol/py.typed
