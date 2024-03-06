9b912f94faa6f43e7cfbb32db2b68a55c3c76fac
Sebastiaan la Fleur <sebastiaan.lafleur@tno.nl>
2024-03-06 11:35:53 +0100

    10: Add default protoc includes to repo.

--- ci/linux/gen_protocol.sh
+++ ci/linux/gen_protocol.sh
@@ -6,7 +6,7 @@ fi
 
 rm -Rf python/src/omotes_sdk_protocol/
 mkdir -p python/src/omotes_sdk_protocol/
-protoc -I src/ --python_out python/src/omotes_sdk_protocol/ ./src/job.proto
-protoc -I src/ --mypy_out python/src/omotes_sdk_protocol/ ./src/job.proto
+protoc -I include/ -I src/ --python_out python/src/omotes_sdk_protocol/ ./src/job.proto
+protoc -I include/ -I src/ --mypy_out python/src/omotes_sdk_protocol/ ./src/job.proto
 touch python/src/omotes_sdk_protocol/__init__.py
 touch python/src/omotes_sdk_protocol/py.typed
