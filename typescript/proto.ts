23ab9fecce0269d835910406f45c79d230c6853b
Wouter Spaak <w.spaak@gmail.com>
2024-03-14 13:40:44 +0100

    Typescript: Add typescript generation Fixes #3

---
+++ typescript/proto.ts
@@ -0,0 +1,14 @@
+import { exec } from 'node:child_process';
+
+const options = [
+  `--plugin=protoc-gen-ts="./node_modules/.bin/protoc-gen-ts"`,
+  `--experimental_allow_proto3_optional`,
+  `--ts_opt=esModuleInterop=true`,
+  `--js_out="import_style=commonjs,binary:./src"`,
+  `--ts_out="./src"`,
+  `--proto_path="../src"`,
+  `job.proto`
+];
+
+const script = `protoc ${options.join(' ')}`;
+exec(script);
