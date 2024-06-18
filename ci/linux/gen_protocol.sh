8351c5eca720628388961ec604b592b560143cdd
Mark Vrijlandt <mark.vrijlandt@tno.nl>
2024-06-18 15:44:40 +0200

    add work flow protocol

--- ci/linux/gen_protocol.sh
+++ ci/linux/gen_protocol.sh
@@ -8,5 +8,7 @@ rm -Rf python/src/omotes_sdk_protocol/
 mkdir -p python/src/omotes_sdk_protocol/
 protoc -I include/ -I src/ --python_out python/src/omotes_sdk_protocol/ ./src/job.proto
 protoc -I include/ -I src/ --mypy_out python/src/omotes_sdk_protocol/ ./src/job.proto
+protoc -I include/ -I src/ --python_out python/src/omotes_sdk_protocol/ ./src/work_flow.proto
+protoc -I include/ -I src/ --mypy_out python/src/omotes_sdk_protocol/ ./src/work_flow.proto
 touch python/src/omotes_sdk_protocol/__init__.py
 touch python/src/omotes_sdk_protocol/py.typed
