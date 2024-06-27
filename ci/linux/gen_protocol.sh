7ddd1682f9ddce0bbb70043b271b4a0dc8d2cc56
Mark Vrijlandt <mark.vrijlandt@tno.nl>
2024-06-27 12:44:31 +0200

    workflow messages per parameter type

--- ci/linux/gen_protocol.sh
+++ ci/linux/gen_protocol.sh
@@ -8,7 +8,7 @@ rm -Rf python/src/omotes_sdk_protocol/
 mkdir -p python/src/omotes_sdk_protocol/
 protoc -I include/ -I src/ --python_out python/src/omotes_sdk_protocol/ ./src/job.proto
 protoc -I include/ -I src/ --mypy_out python/src/omotes_sdk_protocol/ ./src/job.proto
-protoc -I include/ -I src/ --python_out python/src/omotes_sdk_protocol/ ./src/work_flow.proto
-protoc -I include/ -I src/ --mypy_out python/src/omotes_sdk_protocol/ ./src/work_flow.proto
+protoc -I include/ -I src/ --python_out python/src/omotes_sdk_protocol/ ./src/workflow.proto
+protoc -I include/ -I src/ --mypy_out python/src/omotes_sdk_protocol/ ./src/workflow.proto
 touch python/src/omotes_sdk_protocol/__init__.py
 touch python/src/omotes_sdk_protocol/py.typed
