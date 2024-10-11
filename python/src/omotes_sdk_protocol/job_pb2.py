97e83edc99bc30aec15f83bccd32a159ff3188ad
Sebastiaan la Fleur <sebastiaan@lfse.nl>
2024-10-11 11:52:10 +0200

    20: Add job reference to job submission.

--- python/src/omotes_sdk_protocol/job_pb2.py
+++ python/src/omotes_sdk_protocol/job_pb2.py
@@ -15,7 +15,7 @@ _sym_db = _symbol_database.Default()
 from google.protobuf import struct_pb2 as google_dot_protobuf_dot_struct__pb2
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\tjob.proto\x1a\x1cgoogle/protobuf/struct.proto\"\x98\x01\n\rJobSubmission\x12\x0c\n\x04uuid\x18\x01 \x01(\t\x12\x17\n\ntimeout_ms\x18\x02 \x01(\rH\x00\x88\x01\x01\x12\x15\n\rworkflow_type\x18\x03 \x01(\t\x12\x0c\n\x04\x65sdl\x18\x04 \x01(\t\x12,\n\x0bparams_dict\x18\x05 \x01(\x0b\x32\x17.google.protobuf.StructB\r\n\x0b_timeout_ms\"\xc1\x01\n\tJobResult\x12\x0c\n\x04uuid\x18\x01 \x01(\t\x12*\n\x0bresult_type\x18\x02 \x01(\x0e\x32\x15.JobResult.ResultType\x12\x18\n\x0boutput_esdl\x18\x03 \x01(\tH\x00\x88\x01\x01\x12\x0c\n\x04logs\x18\x04 \x01(\t\"B\n\nResultType\x12\r\n\tSUCCEEDED\x10\x00\x12\x0b\n\x07TIMEOUT\x10\x01\x12\t\n\x05\x45RROR\x10\x02\x12\r\n\tCANCELLED\x10\x03\x42\x0e\n\x0c_output_esdl\"D\n\x11JobProgressUpdate\x12\x0c\n\x04uuid\x18\x01 \x01(\t\x12\x10\n\x08progress\x18\x02 \x01(\x01\x12\x0f\n\x07message\x18\x03 \x01(\t\"\xa0\x01\n\x0fJobStatusUpdate\x12\x0c\n\x04uuid\x18\x01 \x01(\t\x12*\n\x06status\x18\x02 \x01(\x0e\x32\x1a.JobStatusUpdate.JobStatus\"S\n\tJobStatus\x12\x0e\n\nREGISTERED\x10\x00\x12\x0c\n\x08\x45NQUEUED\x10\x01\x12\x0b\n\x07RUNNING\x10\x02\x12\x0c\n\x08\x46INISHED\x10\x03\x12\r\n\tCANCELLED\x10\x04\"\x19\n\tJobCancel\x12\x0c\n\x04uuid\x18\x01 \x01(\tb\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\tjob.proto\x1a\x1cgoogle/protobuf/struct.proto\"\xc6\x01\n\rJobSubmission\x12\x0c\n\x04uuid\x18\x01 \x01(\t\x12\x17\n\ntimeout_ms\x18\x02 \x01(\rH\x00\x88\x01\x01\x12\x15\n\rworkflow_type\x18\x03 \x01(\t\x12\x0c\n\x04\x65sdl\x18\x04 \x01(\t\x12,\n\x0bparams_dict\x18\x05 \x01(\x0b\x32\x17.google.protobuf.Struct\x12\x1a\n\rjob_reference\x18\x06 \x01(\tH\x01\x88\x01\x01\x42\r\n\x0b_timeout_msB\x10\n\x0e_job_reference\"\xc1\x01\n\tJobResult\x12\x0c\n\x04uuid\x18\x01 \x01(\t\x12*\n\x0bresult_type\x18\x02 \x01(\x0e\x32\x15.JobResult.ResultType\x12\x18\n\x0boutput_esdl\x18\x03 \x01(\tH\x00\x88\x01\x01\x12\x0c\n\x04logs\x18\x04 \x01(\t\"B\n\nResultType\x12\r\n\tSUCCEEDED\x10\x00\x12\x0b\n\x07TIMEOUT\x10\x01\x12\t\n\x05\x45RROR\x10\x02\x12\r\n\tCANCELLED\x10\x03\x42\x0e\n\x0c_output_esdl\"D\n\x11JobProgressUpdate\x12\x0c\n\x04uuid\x18\x01 \x01(\t\x12\x10\n\x08progress\x18\x02 \x01(\x01\x12\x0f\n\x07message\x18\x03 \x01(\t\"\xa0\x01\n\x0fJobStatusUpdate\x12\x0c\n\x04uuid\x18\x01 \x01(\t\x12*\n\x06status\x18\x02 \x01(\x0e\x32\x1a.JobStatusUpdate.JobStatus\"S\n\tJobStatus\x12\x0e\n\nREGISTERED\x10\x00\x12\x0c\n\x08\x45NQUEUED\x10\x01\x12\x0b\n\x07RUNNING\x10\x02\x12\x0c\n\x08\x46INISHED\x10\x03\x12\r\n\tCANCELLED\x10\x04\"\x19\n\tJobCancel\x12\x0c\n\x04uuid\x18\x01 \x01(\tb\x06proto3')
 
 
 
@@ -65,17 +65,17 @@ if _descriptor._USE_C_DESCRIPTORS == False:
 
   DESCRIPTOR._options = None
   _JOBSUBMISSION._serialized_start=44
-  _JOBSUBMISSION._serialized_end=196
-  _JOBRESULT._serialized_start=199
-  _JOBRESULT._serialized_end=392
-  _JOBRESULT_RESULTTYPE._serialized_start=310
-  _JOBRESULT_RESULTTYPE._serialized_end=376
-  _JOBPROGRESSUPDATE._serialized_start=394
-  _JOBPROGRESSUPDATE._serialized_end=462
-  _JOBSTATUSUPDATE._serialized_start=465
-  _JOBSTATUSUPDATE._serialized_end=625
-  _JOBSTATUSUPDATE_JOBSTATUS._serialized_start=542
-  _JOBSTATUSUPDATE_JOBSTATUS._serialized_end=625
-  _JOBCANCEL._serialized_start=627
-  _JOBCANCEL._serialized_end=652
+  _JOBSUBMISSION._serialized_end=242
+  _JOBRESULT._serialized_start=245
+  _JOBRESULT._serialized_end=438
+  _JOBRESULT_RESULTTYPE._serialized_start=356
+  _JOBRESULT_RESULTTYPE._serialized_end=422
+  _JOBPROGRESSUPDATE._serialized_start=440
+  _JOBPROGRESSUPDATE._serialized_end=508
+  _JOBSTATUSUPDATE._serialized_start=511
+  _JOBSTATUSUPDATE._serialized_end=671
+  _JOBSTATUSUPDATE_JOBSTATUS._serialized_start=588
+  _JOBSTATUSUPDATE_JOBSTATUS._serialized_end=671
+  _JOBCANCEL._serialized_start=673
+  _JOBCANCEL._serialized_end=698
 # @@protoc_insertion_point(module_scope)
